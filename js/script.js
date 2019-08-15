978c36228f8d003383fb3d0b40309515f38d9555
namlt3820 <namlt3820@gmail.com>
2019-08-15 13:53:39 +0700

    feat: site partners

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-    // init Wow
-    new WOW({ offset: 100 }).init()
+	// init Wow
+	new WOW({ offset: 100 }).init();
 
-    $(window).on('scroll', function() {
-        // shrink Navbar
-        if ($(document).scrollTop() > 50) {
-            $('.site__header').addClass('shrink')
-        } else {
-            $('.site__header').removeClass('shrink')
-        } // end shrink Navbar
-    })
-})
+	// Shrink Navbar
+	$(window).on('scroll', function() {
+		if ($(document).scrollTop() > 50) {
+			$('.site__header').addClass('shrink');
+		} else {
+			$('.site__header').removeClass('shrink');
+		}
+	}); // End Shrink Navbar
+
+	// Scroll
+	const OFFSET = 100;
+	$('.navbar li a').click(function(event) {
+		event.preventDefault();
+		$($(this).attr('href'))[0].scrollIntoView();
+		scrollBy(0, -OFFSET);
+	}); // End Scroll
+});
